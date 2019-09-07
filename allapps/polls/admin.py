d3b1fdb7ffa740f723728eaa9beec58117f5c3b2
Aniket Aryamane <aryamane.aniket@gmail.com>
2019-09-07 12:59:51 +0530

    Finishing up models and admin

--- allapps/polls/admin.py
+++ allapps/polls/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Question, Choice
+
+
+admin.site.site_header = 'Pollster Admin'
+admin.site.site_title = 'Pollster Admin'
+admin.site.index_title = 'Pollster Administration'
+
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': [
+                'question_text'
+            ],
+        }),
+        ('Date Information', {
+            'fields': [
+                'pub_date'
+            ],
+            'classes': [
+                'collapse'
+            ],
+        }),
+    ]
+
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
